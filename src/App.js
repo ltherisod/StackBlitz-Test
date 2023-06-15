f70cc009a92076a9ace0bb16e299301b0bb5ee5b
ltherisod <lauratherisod@gmail.com>
2023-06-15 13:55:56 -0300

    images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './style.css';
-import Cronometro from './components/Cronometro';
-import { Practica3 } from './components/Ivan';
+import cat from './img/neko2.png'
 export default function App() {
   return (
     <div>
-      <h1>Hello Ivan!</h1>
-      <Cronometro />
-      <Practica3 />
+      <h1>Imagenes en react!</h1>
+        <p>Imagen desde SRC: la importamos</p>
+        <img src={cat} alt='catsrc'/>
+        <p>Imagen desde PUBLIC : ponemos la direccion como string, no se menciona la carpeta public</p>
+        <img src='../img/caterror.png' alt='catpublic'/>
     </div>
   );
 }
